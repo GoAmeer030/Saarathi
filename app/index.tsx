cc994805b9d095eee92fe78c688145baf612beec
Ameer Noufil <ameer.noufil@Ameer-Noufil-DWWHQJYPH2.local>
2024-11-17 22:21:32 +0530

    tailwind config

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function IndexScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl text-black font-bold">Hello World</Text>
+    </View>
+  );
+}
